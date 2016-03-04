a5cdff9526735fa72ea09a8bb68733d12408acc3
Nick Hufton <nhufton@gmail.com>
2016-03-03 20:34:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# S3 Multipart Tool
+Provides quick access to list/remove unseen multipart objects, contained within a S3 bucket
+
+## Installation
+
+Install prerequisites
+
+* sudo pip install --upgrade pip
+* sudo pip install -U boto3
+
+
+Create credentials file for S3
+The default location for the credentials file is:
+
+* ~/.aws/credentials (Linux/Mac)
+
+The basic format of the credentials file is:
+[default]
+aws\_access\_key\_id = ACCESS\_KEY
+aws\_secret\_access\_key = SECRET\_KEY
+
+
+## Usage
+```
+list buckets: python ./aws_multipart_tool.py --list-buckets
+list parts: python ./aws_multipart_tool.py --list --bucket <bucket-names>
+delete parts: python ./aws_multipart_tool.py --delete --bucket <bucket-names>
+```
---
+++ s3_multipart_tool.py
@@ -0,0 +1,87 @@
+import argparse
+import boto3
+import sys
+
+s3 = boto3.resource('s3')
+
+
+hdd_units =["bytes", "KB", "MB", "GB", "TB", "PB"]
+
+
+def convertDiskSizeToSI( bytes ):
+    index = 0
+    while bytes >= 1024:
+        bytes /= 1024
+        index += 1
+
+    return str(bytes) + " " + hdd_units[index]
+
+
+def output( string ):
+    print(string)
+
+
+def list_buckets():
+    try:
+        count = 1
+        for bucket in s3.buckets.all():
+            print(str(count) + ") " + bucket.name)
+            count += 1
+    except Exception as e:
+        print(e)
+
+
+def list_multiparts(bucket_names):
+    try:
+        for name in bucket_names:
+            bucket = s3.Bucket(name)
+            bucket_size = 0
+
+            for upload in bucket.multipart_uploads.all():
+                multipart_size = 0
+
+                for part in upload.parts.all():
+                    output("   " + str(part.last_modified) + " -- " + convertDiskSizeToSI(part.size) + " -- " + upload.key + " -- " + part.e_tag)
+                    multipart_size += part.size
+
+                bucket_size += multipart_size
+
+            output("** " + convertDiskSizeToSI(bucket_size) + " -- " + bucket.name)
+    except Exception as e:
+        print(e)
+
+
+def delete_multiparts(bucket_names):
+    try:
+        for name in bucket_names:
+            bucket = s3.Bucket(name)
+
+            for upload in bucket.multipart_uploads.all():
+                print("  Deleting " + upload.key)
+                upload.abort()
+    except Exception as e:
+        print(e)
+
+
+def addArguments():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+
+    parser.add_argument('-b', '--bucket', dest='buckets', action='store', nargs='+', help='Specify S3 bucket(s) to use')
+    parser.add_argument('-d', '--delete', dest='delete', action='store_true', help='Delete the multiparts in the specified bucket(s)')
+    parser.add_argument('-l', '--list', dest='list', action='store_true', help='List the multiparts in the specified bucket(s)')
+    parser.add_argument('--list-buckets', dest='list_buckets', action='store_true', help='List all the buckets in the account')
+    
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = addArguments()
+
+    if args.list_buckets:
+        list_buckets()
+
+    if args.delete:
+        delete_multiparts(args.buckets)
+
+    if args.list:
+        list_multiparts(args.buckets)
